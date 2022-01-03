a5c5da9a6e1101581a7b5dd0b4eaf0c97df45278
peggy.nikolaus <peggy.nikolaus@telekom.de>
2022-01-03 09:10:43 +0100

    Challenge erstellt

---
+++ challenge.js
@@ -0,0 +1,7 @@
+var v_nameOfmovie = "Fantastic Beasts and Where to Find Them"
+var v_nameOfdirector = "David Yates"
+var v_yearReleased = "2016"
+
+var v_message = "My favourite film is:" + " " + v_nameOfmovie + " " + "directed by" + " " + v_nameOfdirector + " " + "was released in" + v_yearReleased + "!";
+
+alert(v_message);
